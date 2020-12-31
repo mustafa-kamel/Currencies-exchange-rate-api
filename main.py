9b4ccb3504639c3b4d7859c4e383f1e0b1834299
Mustafa Kamel <mostafakamel72@yahoo.com>
2020-12-31 23:39:07 +0200

    Rename mydbhandle to ratedbhandle

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import datetime
 import requests
-import mydbhandle
+import ratedbhandle
 
 url = "https://api.frankfurter.app"
 
