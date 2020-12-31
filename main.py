e05fc7b1c99c0bc244708fa932eb606814a12109
Mustafa Kamel <mostafakamel72@yahoo.com>
2020-12-31 05:15:27 +0200

    converting the passed currencies to upper case before using them

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app = FastAPI()
 # Initialize the get_rate funcion when a getting a GET request on /rate
 @app.get('/rate')
 async def get_rate(from_currency: str, to_currency: str, date: datetime.date):
-    # Convert the currencies to upper case before using it
+    # Converting the passed currencies to upper case before using them
     from_currency = from_currency.upper()
     to_currency = to_currency.upper()
 
