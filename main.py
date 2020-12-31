3d0ccf53b0a748dd9f49624563f7ebe9a96cece6
Mustafa Kamel <mostafakamel72@yahoo.com>
2020-12-31 05:11:10 +0200

    Adding one more check step on the currencies before requesting the API

--- main.py
+++ main.py
@@ -16,6 +16,16 @@ app = FastAPI()
 # Initialize the get_rate funcion when a getting a GET request on /rate
 @app.get('/rate')
 async def get_rate(from_currency: str, to_currency: str, date: datetime.date):
+    # Check if the passed curriencies is available
+    currencies_response = requests.get("https://api.frankfurter.app/currencies")
+
+    # Parse the json response data
+    currencies = currencies_response.json()
+    if not from_currency in currencies:
+        raise HTTPException(status_code=404, detail=F"The passed currency {from_currency} is not available")
+    elif not to_currency in currencies:
+        raise HTTPException(status_code=404, detail=F"The passed currency {to_currency} is not available")
+
     # Check if the rate is found in the database
     select_query = "SELECT * FROM rates WHERE from_currency = %s AND to_currency = %s AND request_date = %s"
     # Execute the select query
@@ -25,11 +35,10 @@ async def get_rate(from_currency: str, to_currency: str, date: datetime.date):
     # If the rate is found return it
     if result:
         return {'from_currency': result[1], 'to_currency': result[2], 'date': result[3], 'exchange_rate': result[4]}
-        
+
     # Otherwise make a request to get the rate from the API then save it in the database and return it to the client
-    response = requests.get(
-        F"https://api.frankfurter.app/{date}?from={from_currency}&to={to_currency}")
-    
+    response = requests.get(F"https://api.frankfurter.app/{date}?from={from_currency}&to={to_currency}")
+
     # Parse the json response data
     data = response.json()
 
@@ -37,7 +46,7 @@ async def get_rate(from_currency: str, to_currency: str, date: datetime.date):
     insert_query = "INSERT INTO rates (from_currency, to_currency, request_date, exchange_rate) VALUES (%s, %s, %s, %s)"
     row_count = mydbhandle.execute_insert(
         insert_query, (from_currency, to_currency, date, data['rates'][to_currency]))
-    
+
     # Return the data to the client if the insert query succeeded
     if row_count:
         return {'from_currency': from_currency, 'to_currency': to_currency, 'date': date, 'exchange_rate': data['rates'][to_currency]}
