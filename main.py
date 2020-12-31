ec2d433f3378e21d9fb2d4d4e3cb78365de3234a
Mustafa Kamel <mostafakamel72@yahoo.com>
2020-12-31 23:43:54 +0200

    Refactoring main.py and moving some code to raterequesthandler.py

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 from pydantic import BaseModel
 import datetime
-import requests
-import ratedbhandle
+import raterequesthandler
 
-url = "https://api.frankfurter.app"
 
 # Defining a class for the Rate model
 class Rate(BaseModel):
     from_currency: str
     to_currency: str
     date: datetime.date
+
 
 # Create a new app instance
 app = FastAPI()
@@ -19,38 +18,16 @@ app = FastAPI()
 @app.get('/rate')
 async def get_rate(from_currency: str, to_currency: str, date: datetime.date):
     # Converting the passed currencies to upper case before using them
-    from_currency = from_currency.upper()
-    to_currency = to_currency.upper()
-    # Check if the rate is found in the database
-    select_query = "SELECT * FROM rates WHERE from_currency = %s AND to_currency = %s AND request_date = %s"
-    # Execute the select query
-    result = mydbhandle.execute_select(
-        select_query, (from_currency, to_currency, date))
+    from_currency, to_currency = from_currency.upper(), to_currency.upper()
+
+    # Get the rate from the database if it's found
+    result = raterequesthandler.find_indb(from_currency, to_currency, date)
     # If the rate is found return it
     if result:
         return {'from_currency': result[1], 'to_currency': result[2], 'date': result[3], 'exchange_rate': result[4]}
 
-    # Check if the passed curriencies is available
-    currencies_response = requests.get(F"{url}/currencies")
-    # Parse the json response data
-    currencies = currencies_response.json()
-    if not from_currency in currencies:
-        raise HTTPException(status_code=404, detail=F"The passed currency {from_currency} is not available")
-    elif not to_currency in currencies:
-        raise HTTPException(status_code=404, detail=F"The passed currency {to_currency} is not available")
+    # Make a request to check if the passed curriencies is available
+    raterequesthandler.check_currencies_existence(from_currency, to_currency)
 
-    # Otherwise make a request to get the rate from the API then save it in the database and return it to the client
-    response = requests.get(F"{url}/{date}?from={from_currency}&to={to_currency}")
-    data = response.json()
-
-    # Save the response data in the database
-    insert_query = "INSERT INTO rates (from_currency, to_currency, request_date, exchange_rate) VALUES (%s, %s, %s, %s)"
-    row_count = mydbhandle.execute_insert(
-        insert_query, (from_currency, to_currency, date, data['rates'][to_currency]))
-
-    # Return the data to the client if the insert query succeeded
-    if row_count:
-        return {'from_currency': from_currency, 'to_currency': to_currency, 'date': date, 'exchange_rate': data['rates'][to_currency]}
-    # Return HTTP response error if the database failed to save the data
-    else:
-        raise HTTPException(status_code=500, detail="Internal Server Error")
+    # If the rate doesn't exist in the db make a request to get the rate from the API then save it in the database and return it to the client
+    return raterequesthandler.get_and_save_rate(from_currency, to_currency, date)
---
+++ raterequesthandler.py
@@ -0,0 +1,42 @@
+from fastapi import HTTPException
+import requests
+import ratedbhandle
+
+url = "https://api.frankfurter.app"
+
+
+# Get the rate from the database if it's found
+def find_indb(from_currency, to_currency, date):
+    # Check if the rate is found in the database
+    select_query = "SELECT * FROM rates WHERE from_currency = %s AND to_currency = %s AND request_date = %s"
+    # Execute the select query and return the result
+    return ratedbhandle.execute_select(select_query, (from_currency, to_currency, date))
+
+
+# Check if the currencies are provided by the api
+def check_currencies_existence(from_currency, to_currency):
+    currencies_response = requests.get(F"{url}/currencies")
+    # Parse the json response data
+    currencies = currencies_response.json()
+    if not from_currency in currencies:
+        raise HTTPException(status_code=404, detail=F"The passed currency {from_currency} is not available")
+    elif not to_currency in currencies:
+        raise HTTPException(status_code=404, detail=F"The passed currency {to_currency} is not available")
+
+
+# Make a request to get the rate from the api, save it in the database then return in to the user
+def get_and_save_rate(from_currency, to_currency, date):
+    response = requests.get(F"{url}/{date}?from={from_currency}&to={to_currency}")
+    data = response.json()
+
+    # Save the response data in the database
+    insert_query = "INSERT INTO rates (from_currency, to_currency, request_date, exchange_rate) VALUES (%s, %s, %s, %s)"
+    row_count = ratedbhandle.execute_insert(
+        insert_query, (from_currency, to_currency, date, data['rates'][to_currency]))
+
+    # Return the data to the client if the insert query succeeded
+    if row_count:
+        return {'from_currency': from_currency, 'to_currency': to_currency, 'date': date, 'exchange_rate': data['rates'][to_currency]}
+    # Return HTTP response error if the database failed to save the data
+    else:
+        raise HTTPException(status_code=500, detail="Internal Server Error")
