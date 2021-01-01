4768d90c08ae4e73b22e4dc05499ebffa83598f4
Mustafa Kamel <mostafakamel72@yahoo.com>
2021-01-01 04:55:01 +0200

    Updating setup files

--- setup.bat
+++ setup.bat
@@ -1,23 +1,10 @@
-echo 'Starting the installation'
-echo '.....'
+@ECHO OFF
+ECHO 'Starting the installation'
+sleep 1s
 
-echo 'Creating the a virtual environment...'
+ECHO 'Creating a virtual environment'
 python -m venv env
+sleep 2s
 
-sleep 3s
-
-echo 'Activating the virtual environment...'
+ECHO 'Activating the virtual environment'
 env\Scripts\activate.bat
-
-echo 'Installing the requirements...'
-pip install -r requirements.txt
-
-echo 'Initializing db_init.py'
-python db_init.py
-
-echo 'Installation process completed.'
-
-echo 'Creating a dump server'
-uvicorn main:app --reload
-
-echo 'Now visit http://localhost:8000/rate?from_currency=usd&to_currency=eur&date=2021-01-01 to test the server'
--- setup.sh
+++ setup.sh
@@ -1,23 +1,9 @@
 echo 'Starting the installation'
-echo '.....'
+sleep 1s
 
-echo 'Creating the a virtual environment...'
+echo 'Creating a virtual environment...'
 /usr/bin/python3 -m venv env
-
-sleep 3s
+sleep 2s
 
 echo 'Activating the virtual environment...'
 env/bin/activate
-
-echo 'Installing the requirements...'
-env/bin/pip install -r requirements.txt
-
-echo 'Initializing db_init.py'
-/usr/bin/python3 db_init.py
-
-echo 'Installation process completed.'
-
-echo 'Creating a dump server'
-uvicorn main:app --reload
-
-echo 'Now visit http://localhost:8000/rate?from_currency=usd&to_currency=eur&date=2021-01-01 to test the server'
