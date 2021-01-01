71e76bfa0cbfdf7756fc4cdeaef4a68cc27e8352
Mustafa Kamel <mostafakamel72@yahoo.com>
2021-01-01 03:50:49 +0200

    editing the script to receive the database credentials from the user during the setup

--- db_init.py
+++ db_init.py
@@ -2,14 +2,16 @@ import psycopg2
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
 
-def create_database(dbname):
+def create_database(dbname, username, password):
     # Create a connection to the PostgreSQL
     try:
+        print("Establishing a connection to the PostgreSQL Server")
         connection = psycopg2.connect(
-            host='localhost', user="postgres", password="root")
+            host='localhost', user=username, password=password)
         connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
         cursor = connection.cursor()
 
+        print(f"Creating the '{dbname}' database")
         # Create the database with the passed database naem
         create_database_statement = F"CREATE DATABASE {dbname}"
         cursor.execute(create_database_statement)
@@ -26,14 +28,16 @@ def create_database(dbname):
             print("PostgreSQL connection is closed")
 
 
-def create_table(dbname, table_name):
+def create_table(dbname, table_name, username, password):
     # Create a connection to the PostgreSQL
     try:
+        print(f"Establishing a connection to the '{dbname}' database")
         connection = psycopg2.connect(
-            host='localhost', user="postgres", password="root", database=dbname)
+            host='localhost', user=username, password=password, database=dbname)
         connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
         cursor = connection.cursor()
 
+        print(f"Creating the '{table_name}' table")
         # Create a table in passed database with the passed table name
         create_table_statement = F"""CREATE TABLE IF NOT EXISTS {table_name}(
             id serial PRIMARY KEY,
@@ -57,7 +61,9 @@ def create_table(dbname, table_name):
             print("PostgreSQL connection is closed")
 
 
+username = input("Enter the PostgreSQL server username:")
+password = input("Enter the PostgreSQL server password:")
 dbname = "currencies"
 table_name = "rates"
-create_database(dbname)
-create_table(dbname, table_name)
+create_database(dbname, username, password)
+create_table(dbname, table_name, username, password)
